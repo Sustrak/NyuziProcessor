1790e921182d28478a0b225a8583d2d178cea7f8
Jooooc <joel.stv.9@gmail.com>
2022-01-15 12:44:46 +0100

    Random modulo individual modules

---
+++ hardware/core/random_modulo/prng.sv
@@ -0,0 +1,50 @@
+`include "defines.svh"
+
+import defines::*;
+
+//
+//This module implements the Pseudo-Random Number Generator (PRNG)
+//used by the Random Modulo in the L1 caches.
+//
+
+module prng
+    #(parameter SEED = 168'hef4a66be741ca34e9143bfa4c10c4b14af2bb26021,
+    parameter NBITS = 168,
+    parameter NNUM = 16)
+    (input                   clk,
+    input                    reset,
+
+    output logic[SIZE-1:0]   rand_o);
+
+    //Actual code
+    logic[NBITS-1:0] lsfr;
+    logic[NNUM-1:0][3:0] xnor_i;
+    logic[NNUM-1:0] xnor_o;
+
+    assign rand_o = xnor_o;
+
+    genvar i;
+    generate
+        for(i=0; i < NNUM; i++)
+        begin
+            assign xnor_i[i][0] = lsfr[NBITS - NNUM + i];
+            assign xnor_i[i][1] = lsfr[NBITS - NNUM + i - 2];
+            assign xnor_i[i][2] = lsfr[NBITS - NNUM + i - 15];
+            assign xnor_i[i][3] = lsfr[NBITS - NNUM + i - 17];
+            assign xnor_o[i]    = ~(xnor_i[i][0] ^ xnor_i[i][1] ^ xnor_i[i][2] ^ xnor_i[i][3]);
+        end
+    endgenerate
+    
+    always_ff @(posedge clk ) 
+    begin 
+        if(reset)
+            lsfr <= SEED;
+        else begin
+            for(int i = NBITS-1; i >= NNUM; i--)
+                lsfr[i] <= lsfr[i-1];
+            for(int i = NNUM-1; i >= 0; i--)
+                lsfr[i] <= xnor_o[i];
+        end    
+    end
+    
+endmodule
---
+++ hardware/core/random_modulo/random_placement.sv
@@ -0,0 +1,61 @@
+`include "defines.svh"
+
+import defines::*;
+
+//
+//
+
+module random_placement
+    #(parameter INDX_BITS = 6,
+    parameter   ADDR_BITS = 24,
+    parameter   CONT_BITS = 12)
+    (input                      clk,
+    input                       reset,
+
+    input logic                 reseed,
+    input logic[ADDR_BITS-1:0]  raddr,
+    input logic[ADDR_BITS-1:0]  waddr,   
+    output logic[INDX_BITS-1:0] rindex,
+    output logic[INDX_BITS-1:0] windex);
+
+    //Actual code
+    logic[CONT_BITS-1:0] prng_o; //Seed from the PRNG module
+    logic[CONT_BITS-1:0] seed; //Current seed
+    logic[CONT_BITS-1:0] cont_w;
+    logic[CONT_BITS-1:0] cont_r;
+
+    assign cont_w = waddr[INDX_BITS + CONT_BITS - 1 : INDX_BITS] ^ seed;
+    assign cont_r = raddr[INDX_BITS + CONT_BITS - 1 : INDX_BITS] ^ seed;
+
+    prng #(
+        .NNUM(CONT_BITS)
+    ) seed_gen(
+        .clk(clk),
+        .reset(reset),
+        .rand_o(prng_o)
+    );
+
+    benes_network_6 benes_write(
+        .clk(clk),
+        .reset(reset),
+        .word_i(waddr[INDX_BITS-1:0]),
+        .control(cont_w),
+        .word_o(windex)
+    );
+    benes_network_6 benes_read(
+        .clk(clk),
+        .reset(reset),
+        .word_i(raddr[INDX_BITS-1:0]),
+        .control(cont_r),
+        .word_o(rindex)
+    );
+
+    always_ff @(posedge clk) 
+    begin
+        if(reset)
+            seed <= {CONT_BITS{1'b0}};
+        else if(reseed)
+            seed <= prng_o;        
+    end
+
+endmodule
